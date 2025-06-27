c8d50a3ed5762d0cb691d72f2282c6cb0a2e22b5
roblabla <unfiltered@roblab.la>
2025-06-27 11:29:00 +0200

    Auto-elevate installprofile as a service
    The way ConfigurationProfile works is by spawning some XPC "helper"
    processes, who will be in charge of applying the actual payloads. Those
    helpers can be found in the following folder:
    
    /System/Library/PrivateFrameworks/ConfigurationProfiles.framework/XPCServices
    
    Unfortunately, ConfigurationProfile spawns those helpers in the current
    session - which means that even if we run installprofile with sudo, the
    spawned helpers will run under that user, and be limited by its
    permissions. This caused TCCProfileService to fail, as a normal user
    doesn't have the (unix) privileges to modify files in the
    /Library/Application Support/com.apple.TCC folder.
    
    We fix this by elevating installprofile to root through a launchd
    service. It creates a launchd service that respawns itself with a
    special flag (indicating we're running as service), in the correct
    folder, and targeting the right file. It then proceeds to load it in
    launchd, uses launchd debug to redirect the service's stdout/stderr to
    the current terminal, and finally starts it.
    
    Afterwards, it's going to monitor the current processes to figure out
    when the installprofile service stops, kill the launchd debug session,
    and exit.
    
    Note that the way it works is cursed and there is _surely_ a better way
    to do this, but I couldn't find one, and this works well enough.

--- installprofile.m
+++ installprofile.m
@@ -4,6 +4,7 @@
 #define INTERFACES_H
 
 #import <Foundation/Foundation.h>
+#include "libproc.h"
 
 @interface CPProfile : NSObject <NSSecureCoding>
 - (id)initWithData:(id)arg1 error:(id *)arg2;
@@ -21,14 +22,12 @@
 
 #include <stdio.h>
 
-int main() {
-    NSArray *arguments = [[NSProcessInfo processInfo] arguments];
-    if ([arguments count] != 2) {
-        NSLog(@"Usage: %@ <file>", [arguments objectAtIndex:0]);
-        return 1;
-    }
+int usage(NSArray *args) {
+    NSLog(@"Usage: %@ <file>", [args objectAtIndex:0]);
+    return 1;
+}
 
-    NSString *path = [arguments objectAtIndex:1];
+int runAsService(NSString *path) {
     CPProfileManager *mgr = [CPProfileManager sharedProfileManager];
     if (mgr == nil) {
         NSLog(@"Failed to get shared CPProfileManager.");
@@ -59,4 +58,134 @@ int main() {
         NSLog(@" error => %@ ", error);
         return 1;
     }
+    NSLog(@"Successfully installed profile");
+    return 0;
+}
+
+bool installProfileServiceIsUp() {
+    int numberOfProcesses = proc_listpids(PROC_ALL_PIDS, 0, NULL, 0);
+    pid_t pids[numberOfProcesses];
+    bzero(pids, numberOfProcesses);
+    proc_listpids(PROC_ALL_PIDS, 0, pids, sizeof(pids));
+    for (int i = 0; i < numberOfProcesses; ++i) {
+        if (pids[i] == 0) { continue; }
+        if (pids[i] == getpid()) { continue; }
+        char pathBuffer[PROC_PIDPATHINFO_MAXSIZE];
+        bzero(pathBuffer, PROC_PIDPATHINFO_MAXSIZE);
+        proc_pidpath(pids[i], pathBuffer, sizeof(pathBuffer));
+
+        NSString *path = [NSString stringWithUTF8String:pathBuffer];
+        NSString *currentExecutable = [[NSBundle mainBundle] executablePath];
+        if ([[path lastPathComponent] isEqualTo:[currentExecutable lastPathComponent]]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int createAndWaitService(NSString *path) {
+    NSString *currentExecutable = [[NSBundle mainBundle] executablePath];
+    NSDictionary *service = @{
+        @"Label": @"installprofile",
+        @"ProgramArguments": @[currentExecutable, @"-s", path],
+        @"RunAtLoad": @NO,
+        @"WorkingDirectory": [[NSFileManager defaultManager] currentDirectoryPath],
+    };
+
+    NSError *error;
+    NSData *data = [NSPropertyListSerialization dataWithPropertyList:service format:NSPropertyListXMLFormat_v1_0 options:0 error:&error];
+    if (data == nil) {
+        NSLog(@"Failed to serialize service: %@", error);
+        return 1;
+    }
+
+    NSString *servicePath = @"/Library/LaunchDaemons/installprofile.plist";
+    NSLog(@"Creating service at %@ with %@", servicePath, service);
+    if ([data writeToFile:servicePath options:NSDataWritingAtomic error:&error] == false) {
+        NSLog(@"Failed to write service: %@", error);
+        return 2;
+    }
+
+    // Unload previous service
+    NSTask *task = [NSTask new];
+    [task setLaunchPath:@"/bin/launchctl"];
+    [task setArguments:@[@"unload", servicePath]];
+    [task launch];
+    [task waitUntilExit];
+
+    // Load service
+    task = [NSTask new];
+    [task setLaunchPath:@"/bin/launchctl"];
+    [task setArguments:@[@"load", servicePath]];
+    [task launch];
+    [task waitUntilExit];
+    // return code 0 is for loaded daemon, and much info is in stdout, and 113 or other nonzero value when daemon is not loaded
+    if ([task terminationStatus] != 0) {
+        NSLog(@"Failed to load service");
+        return 3;
+    }
+
+    // Start a debug service so we get stdio here.
+    NSString *serviceName = @"system/installprofile";
+    NSTask *debugTask = [NSTask new];
+    [debugTask setLaunchPath:@"/bin/launchctl"];
+    [debugTask setArguments:@[@"debug", serviceName, @"--stdout", @"--stderr"]];
+    [debugTask launch];
+
+    NSTask *startTask = [NSTask new];
+    [startTask setLaunchPath:@"/bin/launchctl"];
+    [startTask setArguments:@[@"kickstart", serviceName, @"-k"]];
+    [startTask launch];
+    [startTask waitUntilExit];
+
+    while (installProfileServiceIsUp()) {
+        sleep(1);
+    }
+    [debugTask terminate];
+    [debugTask waitUntilExit];
+
+    if ([[NSFileManager defaultManager] removeItemAtPath:servicePath error:&error] == false) {
+        NSLog(@"Failed to delete service: %@", error);
+        return 4;
+    }
+    return 0;
+}
+
+int main(int argc, char**argv) {
+    NSArray *arguments = [[NSProcessInfo processInfo] arguments];
+
+    bool isService = getppid() == 1;
+    NSString *path = nil;
+    for (NSUInteger i = 1; i < [arguments count]; i++) {
+        NSString *arg = arguments[i];
+        if ([arg hasPrefix:@"-"]) {
+            NSMutableArray  *chars = [NSMutableArray array];
+
+            [arg enumerateSubstringsInRange:NSMakeRange(1, [arg length] - 1) options: NSStringEnumerationByComposedCharacterSequences
+                usingBlock: ^(NSString *inSubstring, NSRange inSubstringRange, NSRange inEnclosingRange, BOOL *outStop) {
+                [chars addObject:inSubstring];
+            }];
+            for (id c in chars) {
+                if ([c isEqual:@"s"]) {
+                    isService = true;
+                } else {
+                    return usage(arguments);
+                }
+            }
+        } else if (path == nil) {
+            path = arg;
+        } else {
+            return usage(arguments);
+        }
+    }
+    if (path == nil) {
+        return usage(arguments);
+    }
+
+    if (!isService) {
+        // Create service
+        return createAndWaitService(path);
+    }
+
+    return runAsService(path);
 }
